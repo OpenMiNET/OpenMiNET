765f32464571d21b6e6a2191233f7048c7e53d59
Kenny van Vulpen <kennyvvulpen@ziggo.nl>
2019-07-10 19:13:47 +0200

    Fix for latest MiNET

--- src/OpenAPI/World/OpenLevel.cs
+++ src/OpenAPI/World/OpenLevel.cs
@@ -168,7 +168,8 @@ namespace OpenAPI.World
 			return !bb.IsCancelled;
 		}
 
-		public override void BroadcastMessage(string text, MessageType type = MessageType.Chat, MiNET.Player sender = null, MiNET.Player[] sendList = null)
+		public override void BroadcastMessage(string text, MessageType type = MessageType.Chat, MiNET.Player sender = null, MiNET.Player[] sendList = null,
+			bool needsTranslation = false, string[] parameters = null)
 		{
 			if (type == MessageType.Chat || type == MessageType.Raw)
 			{
@@ -179,25 +180,24 @@ namespace OpenAPI.World
 					message.type = 0;
 					message.source = ""; //sender == null ? "" : (sender.DisplayName ?? sender.Username);
 					message.message =$"{sen}{line}";
-				    //message.parameters = new string[0];
-				  //  message.islocalized = false;
+					//message.parameters = new string[0];
+					//  message.islocalized = false;
 
-                    RelayBroadcast(sendList, message);
+					RelayBroadcast(sendList, message);
 				}
 				return;
 			}
 			else
 			{
-			    McpeText message = McpeText.CreateObject();
-			    message.type = (byte)type;
-			    message.source = sender == null ? "" : sender.Username;
-			    message.message = text;
-             //   message.parameters = new string[0];
-			 //   message.islocalized = false;
+				McpeText message = McpeText.CreateObject();
+				message.type = (byte)type;
+				message.source = sender == null ? "" : sender.Username;
+				message.message = text;
+				//   message.parameters = new string[0];
+				//   message.islocalized = false;
 
-			    RelayBroadcast(sendList, message);
+				RelayBroadcast(sendList, message);
 			}
-           // base.BroadcastMessage(text, type, sender, sendList);
 		}
 
 		public override void Close()
